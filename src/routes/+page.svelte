f1f9bfb4ce7276f0c079e22625bdecfecf489592
aquacalc <aquacalc@yahoo.com>
2023-02-03 11:05:45 -0500

    add basic text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,78 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero">
+	<h1>Welcome to the Rachel Carson Network</h1>
+</div>
+
+<div class="step-content">
+  <!-- <section> -->
+    <img src="Homestead.png" alt="Rachel Carson Homestead">
+  <!-- </section> -->
+
+	<section>
+		<p>This site is under construction.</p>
+		<p>
+			Please visit the website of the <a href="https://rachelcarsonhomestead.org/">Rachel Carson Homestead</a>, the
+			organization based in her hometown of <a
+				href="https://www.google.com/maps/place/Rachel+Carson+Homestead/@40.5434951,-79.7895676,15z/data=!4m13!1m7!3m6!1s0x8834950150bdf767:0x71ce632b4904db0!2sSpringdale,+PA!3b1!8m2!3d40.5409008!4d-79.7839373!3m4!1s0x883495aa9f32c149:0x87dd70d27b9abeb6!8m2!3d40.5464262!4d-79.7833167"
+				>Springdale, Western Pennsylvania</a
+			>, that is dedicated to preserving, disseminating, and advancing Rachel Carson's work and environmental ethic.
+		</p>
+	</section>
+
+</div>
+
+<style>
+	:global(body) {
+		overflow-x: hidden;
+
+		background: linear-gradient(
+			to right,
+			rgba(247, 247, 247, 0.65) 70%,
+			rgba(41, 89, 180, 0.65) 250%
+		);
+	}
+
+	.hero {
+		height: 20vh;
+		display: flex;
+		place-items: center;
+		flex-direction: column;
+		justify-content: center;
+		text-align: center;
+		background-color: aliceblue;
+		font-family: sans-serif;
+	}
+
+	.step-content {
+		font-size: 1rem;
+		line-height: 1.618;
+		background: whitesmoke;
+		color: #676666;
+		border-radius: 5px;
+		padding: 0.5rem 1rem;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		transition: background 500ms ease;
+		box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.2);
+		text-align: left;
+		width: 100%;
+		margin: auto;
+		margin-top: 50px;
+		max-width: 800px;
+	}
+
+	section {
+		text-align: left;
+		font-size: 1.75rem;
+		margin: 0rem;
+		line-height: 2.83rem;
+		font-family: sans-serif;
+	}
+
+	h1 {
+		font-family: sans-serif;
+		font-weight: 400;
+		font-size: 3.5rem;
+		color: #555;
+	}
+</style>
